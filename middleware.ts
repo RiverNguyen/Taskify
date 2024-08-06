c9d3e6beaea7fbe5d4a05afe93a102880c1bd1f3
RiverNguyen <ngiang1309@gmail.com>
2024-08-06 13:05:50 +0700

    update middleware and add setting page

--- middleware.ts
+++ middleware.ts
@@ -1,36 +1,36 @@
-import { authMiddleware, redirectToSignIn } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
-export default authMiddleware({
-  publicRoutes: ["/"],
-  afterAuth: (auth, req) => {
-    if (auth.userId && auth.isPublicRoute) {
-      let path = "/select-org";
+const isProtectedRoute = createRouteMatcher([
+  "/organization(.*)",
+  "/board(.*)",
+  "/select-org",
+  "/api/cards(.*)",
+]);
 
-      if (auth.orgId) {
-        path = `/organization/${auth.orgId}`;
-      }
+export default clerkMiddleware((auth, req, event) => {
+  if (!auth().userId && isProtectedRoute(req)) {
+    return auth().redirectToSignIn({ returnBackUrl: req.url });
+  }
 
-      const orgSelection = new URL(path, req.url);
-      return NextResponse.redirect(orgSelection);
+  if (auth().userId && !isProtectedRoute(req)) {
+    let path = "/select-org";
+
+    if (auth().orgId) {
+      path = `/organization/${auth().orgId}`;
     }
+    return NextResponse.redirect(new URL(path, req.url));
+  }
 
-    if (!auth.userId && !auth.isPublicRoute) {
-      return redirectToSignIn({ returnBackUrl: req.url });
-    }
-
-    if (auth.userId && !auth.orgId && req.nextUrl.pathname !== "/select-org") {
-      const orgSelection = new URL("/select-org", req.url);
-      return NextResponse.redirect(orgSelection);
-    }
-  },
+  if (
+    auth().userId &&
+    !auth().orgId &&
+    req.nextUrl.pathname !== "/select-org"
+  ) {
+    return NextResponse.redirect(new URL("/select-org", req.url));
+  }
 });
 
 export const config = {
-  matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    // Always run for API routes
-    "/(api|trpc)(.*)",
-  ],
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
