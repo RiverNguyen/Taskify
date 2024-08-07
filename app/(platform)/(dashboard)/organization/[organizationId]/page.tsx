c04f47ba9c02e7759a2e6f2f152317a383e55172
RiverNguyen <ngiang1309@gmail.com>
2024-08-07 10:48:46 +0700

    learn form server action

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,7 +1,19 @@
-import React from "react";
+import { db } from "@/lib/db";
+import { Board } from "./board";
+import { Form } from "./form";
 
-const OrganizationIdPage = () => {
-  return <div>Organization Page</div>;
+const OrganizationIdPage = async () => {
+  const boards = await db.board.findMany();
+  return (
+    <div className="flex flex-col">
+      <Form />
+      <div className="space-y-2">
+        {boards?.map((board) => (
+          <Board key={board.id} id={board.id} title={board.title} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default OrganizationIdPage;
